2a71a6eeb820df8581f955c8777c6ab8e6718999
Manish  Mishra <manishweb9199@gmail.com>
2024-12-15 02:07:04 +0530

    Add files via upload

---
+++ 03_data-type.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+  int a =1;
+
+float b;
+b = 1.4;
+char c;
+c = 'a';
+
+printf("The value of a is %d",a);
+printf("The value of b is %f", b);
+printf("The value of a is %d",a);
+}
