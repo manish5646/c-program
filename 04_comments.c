0203054a4d6ed819b50e07bc3d0e5f0c7e3af31f
Manish  Mishra <manishweb9199@gmail.com>
2024-12-15 02:08:09 +0530

    Add files via upload

---
+++ 04_comments.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+  int main(){
+    //I am writing this just for fun. this line doesnt do anything;
+    /*i am here to define the thing .
+    have a wonderful day.*/
+    printf("Hello World");
+    return 0;
+  }
